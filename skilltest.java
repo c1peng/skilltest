55a8c8dde97dbf5e977ba64701b7cdafe743e888
Chuan Peng <c1peng@ucsd.edu>
2022-02-11 13:20:45 -0800

    Update skilltest.java

--- skilltest.java
+++ skilltest.java
@@ -2,4 +2,8 @@ public class skilltest {
     public static int minus(int a, int b) {
         return (a-b);
     }
+
+    public static void main(String[] args) {
+        System.out.print(minus(5, 3));
+    }
 }
